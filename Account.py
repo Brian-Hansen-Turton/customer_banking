aafb3dc3d3564f98c1d106ac73a6e83df6f17176
Brian-Hansen-Turton <britine@me.com>
2024-08-20 18:15:16 -0400

    removed starter files

--- Account.py
+++ Account.py
@@ -2,16 +2,22 @@
 
 class Account:
     """Creating an Account class with methods"""
-    def __init__(self, balance, interest):
-        self.balance = balance
-        self.interest = interest
+    def __init__(self, balance, interest = 0):
+        self.__balance__ = balance
+        self.__interest__ = interest
 
-    # This method sets the balance of the account.
     def set_balance(self, balance):
         """Sets the balance for the for the account"""
-        self.balance = balance
+        self.__balance__ = balance
 
-    # The method sets the interest gained for the account.
+    def get_balance(self): 
+        """Gets the balance for the for the account"""
+        return self.__balance__
+
     def set_interest(self, interest):
         """Sets the interest gained for the the account"""
-        self.interest = interest
+        self.__interest__ = interest
+
+    def get_interest(self): 
+        """Gets the interest gained for the the account"""
+        return self.__interest__   
